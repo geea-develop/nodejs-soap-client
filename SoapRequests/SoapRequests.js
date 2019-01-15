e8a2c482e9e05259ccf68bafb383c881742b28b6
Guy Ambar <guy@meetleo.co>
2019-01-15 13:35:50 +0200

    * dynamic xml

---
+++ SoapRequests/SoapRequests.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var index_1 = require("./index");
+exports.SoapRequests = {
+    PfxRequest: index_1.PfxRequest,
+    SoapRequest: index_1.SoapRequest
+};
